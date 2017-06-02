d79e445127863348d76e58291eaee9e663b686f5
unknown <rrica@users.noreply.github.com>
2017-06-02 14:40:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// change background color to red
+document.body.style.backgroundColor = "red";
+
+//Change the background color of just the "about me" portion to blue
+document.getElementById("about me").style.backgroundColor = "blue";
+
+//Change the font-family of the entire document to sans-serif
+document.body.style.fontFamily = "sans-serif";
+
+//Add your nickname
+document.getElementById("nickname").innerHTML = "Rici"
+
+//Add your favorites
+document.getElementById("favorites").innerHTML = "Hiking, biking, exploring, reading"
+
+//Add your hometown
+document.getElementById("hometown").innerHTML = "Germany"
+
+//Iterate through each li and change the class to "listitem". Add a style tag that sets a 
+//rule for "listitem" to make the color red.
+
+li = document.body.getElementsByTagName('li');
+for (li == true) {
+    class = "listitem"
+	
+}
+//Create a new img element and set its src attribute to a picture of you. Append that element to the page.
